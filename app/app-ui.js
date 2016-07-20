b6c103ad45d0d8f43e78a79a7274c9f0282b409a
Sergiy <serparkhuts@gmail.com>
2016-07-20 11:01:38 +0300

    connected all partials

--- app/app-ui.js
+++ app/app-ui.js
@@ -15,18 +15,32 @@ routerApp.config(function($routeProvider,$stateProvider, $urlRouterProvider) {
       //templateUrl: 'cities-list/cities-list.html'
       template: '<cities-list></cities-list>'
     })
-
-    .state('/details/:cityId.daily', {
-        url: '/daily',
-        templateUrl: 'partials/partial-daily.html'
-        
-    })
     .state("/details/:cityId.now",{
       url: "/now",
       templateUrl: "partials/partial-now.html"
     })
+    .state('/details/:cityId.daily', {
+      url: '/daily',
+      templateUrl: 'partials/partial-daily.html'
+    })
     .state("/details/:cityId.hourly",{
       url: "/hourly",
       templateUrl: "partials/partial-hourly.html"
+    })
+    .state("/details/:cityId.morning",{
+      url:"/morning",
+      templateUrl:"partials/partial-morning.html"
+    })
+    .state("/details/:cityId.afternoon",{
+      url:"/afternoon",
+      templateUrl:"partials/partial-afternoon.html"
+    })
+    .state("/details/:cityId.evening",{
+      url:"/evening",
+      templateUrl:"partials/partial-evening.html"
+    })
+    .state("/details/:cityId.overnight",{
+      url:"/overnight",
+      templateUrl:"partials/partial-overnight.html"
     });
 });
