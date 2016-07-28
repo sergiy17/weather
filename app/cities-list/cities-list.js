da3ce7f1d0ffb776500cbee3889ab882ccb4bb87
Sergiy <serparkhuts@gmail.com>
2016-07-28 17:36:25 +0300

    done with FUSE

--- app/cities-list/cities-list.js
+++ app/cities-list/cities-list.js
@@ -5,9 +5,6 @@ function fetchCities($http){
 		return response.data;
 	});
 }
-
-
-
 function controller($http, $scope, $state, $timeout){
 	var model = this;
 	$scope.currentCity;
@@ -26,47 +23,26 @@ function controller($http, $scope, $state, $timeout){
 	};
 	function redirect(selected_item){
 		$scope.currentCity = selected_item.value;
-		// console.log(selected_item);
 		$scope.goToCity($scope.currentCity);
 	};
-	// Fuse start
-		var books = ["Old Man's War","a", "The Lock Artist", "HTML5", "Right Ho Jeeves"];
-
-		var f = new Fuse(books);
-		var result = f.search('The');
-		// console.log(result);
-	// Fuse end
-
 	// 'term' <= what was entered
 	function suggest_city(term) {
-		// Fuse start
-		// var books = ["Old Man's War","a", "The Lock Artist", "HTML5", "Right Ho Jeeves"];
-		// var f = new Fuse(books);
-		// var result = f.search(term);
-		// console.log("+++++++++++++++++++res "+result);
-		// console.log(result);
-		// Fuse end
-		console.log(term);
 		var q = term.toLowerCase().trim();
 		var results = [];
 		var options = {
 		  keys: ['name'],
-		  threshold: 0.8
+		  id: name,
+		  threshold: 0.1
 		}
-		console.log('arr of obj '+model.arrOfObj.length);
+		console.log('arr of obj '+model.arrOfObj.length	);
 	// Find first 10 cities that start with `term`.
 		var f = new Fuse(model.arrOfObj, options);
-
+		var resCities = [];
 		var results = f.search(q);
-
-		console.log(results.length);
-	  // for (var i = 0; i < model.arrOfObj.length && results.length < 15; i++) {
-	  //   var city = model.arrOfObj[i];
-	  //   console.log(city.name);
-	  //   if (city.name.toLowerCase().indexOf(q) === 0)
-	  //     results.push({ label: city.name, value:city._id});
-	  // }
-  	return results;
+		for(var ii=0;ii<results.length;ii++){
+			resCities.push({label: results[ii].name, value:results[ii]._id});
+		};
+  	return resCities;
   };
   // 'suggest_city' <= Entered value
   // 'on_select:redirect' <= Direction of redirecting while click on link
@@ -74,7 +50,6 @@ function controller($http, $scope, $state, $timeout){
   	suggest:suggest_city,
   	on_select:redirect
   };
-
 };
 
 module.component("citiesList",{
