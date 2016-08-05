bae39c1b7499c6c645a47427cf667430633c2f9f
Sergiy <serparkhuts@gmail.com>
2016-08-05 10:13:23 +0300

    resolved small mistake

--- app/info-window/info-window.js
+++ app/info-window/info-window.js
@@ -3,7 +3,7 @@ var infoModule = angular.module('weatherLib');
 controller = function($scope, $stateParams,functionsSrvc){
 	var model = this;
 
-	 setData = function(data){S
+	 setData = function(data){
 		var arr4Temp = [];
 		for(var i=0;i<10;i++){
 			arr4Temp.push(Math.round(functionsSrvc.kelvinToCelsius(data.list[i].main.temp)));
