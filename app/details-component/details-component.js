d6999e40788fd613d2016e6f03b7aaf6bcf88cf4
Sergiy <serparkhuts@gmail.com>
2016-08-17 15:02:26 +0300

    serverSrvc changed to http.get

--- app/details-component/details-component.js
+++ app/details-component/details-component.js
@@ -18,75 +18,76 @@ function requestToApi($scope, functionsSrvc,serverSrvc, $log, $timeout){
 		model.stateP.lat;
 		model.stateP.lon;
 		$log.debug("weatherLib.requestToApi");
-	$timeout(function(){
+		// console.group(model.stateP.cityId);
+
 		if(!model.stateP.lon && !model.stateP.cityId){
 			functionsSrvc.getLocation().then(function(geoPosition){
 				model.stateP.lat = geoPosition.lat;
 				model.stateP.lon = geoPosition.lon;
-				promise = serverSrvc.getData(model.stateP.lat, model.stateP.lon, model.stateP.cityId).then(function(data){parseData(data)});;
+				serverSrvc.getData(model.stateP.lat, model.stateP.lon, model.stateP.cityId).then(function(weatherData){
+					parseData(weatherData);
+				});
 			});
 		}
-		// if we have cityId
 		else {
-			promise = serverSrvc.getData(model.stateP.lat, model.stateP.lon, model.stateP.cityId).then(function(data){parseData(data)});
+			console.log("with cityId");
+			serverSrvc.getData(model.stateP.lat, model.stateP.lon, model.stateP.cityId).then(function(weatherData){
+				parseData(weatherData);
+			});
 		}
 
-		parseData = function(data){
-			model.respData = data;
-		  model.cityName =  model.respData.city.name;
-		  model.Data = model.respData.list.map(function(i){
-		    return i;
-		  });
-		  // model.Data = allData;
+	parseData = function(data){
+		model.respData = data;
+	  model.cityName =  model.respData.city.name;
+	  model.Data = model.respData.list.map(function(i){
+	    return i;
+	  });
 
-		  for(var i=0;i<10;i++){
-		  	model.Data[i].dt = model.Data[i].dt * 1000;
+	  for(var i=0;i<10;i++){
+	  	model.Data[i].dt = model.Data[i].dt * 1000;
+	  }
+	  
+		for(var i=0;i<10;i++){
+			var a = model.respData.list[i].dt_txt;
+			a = a.slice(11,16);
+			timeArr.push(a);
+		}
+		
+		timeArr.slice(0,10);
+
+		for(var i=0;i<10;i++){
+			var arr4temp = [];
+			arr4temp = Math.round(model.Data[i].main.temp);
+			tempArr.push(arr4temp);
+		}
+
+		var ctx = document.getElementById("myChart");
+		var timeD = timeArr;
+		var myChart = new Chart(ctx, {
+		  type: 'bar',
+		  data: {
+		    labels: timeD,
+		    datasets: [{
+		      label: 'Temperature for '+model.cityName,
+		      data: tempArr,
+		      backgroundColor: 'rgba(54, 162, 235, 1)',
+		      borderColor: 'rgba(54, 162, 235, 1)',
+		      borderWidth: 1
+		    }]
+		  },
+		  options: {
+		    scales: {
+		      yAxes: [{
+		        ticks: {
+		          beginAtZero:true
+		        }
+		      }]
+		    }
 		  }
-		  
-			for(var i=0;i<10;i++){
-				var a = model.respData.list[i].dt_txt;
-				a = a.slice(11,16);
-				timeArr.push(a);
-			}
-			
-			timeArr.slice(0,10);
-
-			for(var i=0;i<10;i++){
-				var arr4temp = [];
-				arr4temp = Math.round(model.Data[i].main.temp);
-				tempArr.push(arr4temp);
-			}
-
-			var ctx = document.getElementById("myChart");
-			var timeD = timeArr;
-			var myChart = new Chart(ctx, {
-			  type: 'bar',
-			  data: {
-			    labels: timeD,
-			    datasets: [{
-			      label: 'Temperature for '+model.cityName,
-			      data: tempArr,
-			      backgroundColor: 'rgba(54, 162, 235, 1)',
-			      borderColor: 'rgba(54, 162, 235, 1)',
-			      borderWidth: 1
-			    }]
-			  },
-			  options: {
-			    scales: {
-			      yAxes: [{
-			        ticks: {
-			          beginAtZero:true
-			        }
-			      }]
-			    }
-			  }
-			});
-		// angular.copy(this.cityName,model.cityName);
-		};
-	});
+		});
+	// angular.copy(this.cityName,model.cityName);
+	};
 };
-
-
 
 module.run(function($rootScope){
   $rootScope
--- app/services/server/serverSrvc.js
+++ app/services/server/serverSrvc.js
@@ -1,31 +1,24 @@
 var module = angular.module("weatherLib");
 
 module.provider("serverSrvc",function(){
-	this.$get = function($q,$log){
+	this.$get = function($q,$log,$http){
 		var lang = this.lang;
 		return {
 			getData : function(lat,lon, cityId){
 				var promise = $q.defer();
-				var xhttp = new XMLHttpRequest();
-				xhttp.onreadystatechange = function(){
-					if(xhttp.readyState == 4 && xhttp.status == 200){
-						var resp = JSON.parse(xhttp.responseText);
-						promise.resolve(resp);
-					}
-				}
-				$log.debug("weatherLib.serverSrvc.$get");
 				if(cityId){
-					xhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast?id=" + cityId + "&mode=json&units=metric&lang="+lang+"&cnt=10&appid=0c853911efc43a5ce9db3e839f13abc9", true);
-					xhttp.send();
-					return promise.promise;
+					$http.get('http://api.openweathermap.org/data/2.5/forecast?id=' + cityId + '&mode=json&units=metric&lang='+lang+'&cnt=10&appid=0c853911efc43a5ce9db3e839f13abc9').success(function(data){
+						promise.resolve(data);
+					});
+
+				}else{
+					$http.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&mode=json&units=metric&lang='+lang+'&cnt=10&appid=0c853911efc43a5ce9db3e839f13abc9').success(function(data){
+						promise.resolve(data);
+					});
 				}
-				else{
-					xhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&mode=json&units=metric&lang="+lang+"&cnt=10&appid=0c853911efc43a5ce9db3e839f13abc9", true);
-					xhttp.send();
 				return promise.promise;
-				}
 			}		
-		}
+		};
 	};
 
 	this.setLanguage = function(language){
